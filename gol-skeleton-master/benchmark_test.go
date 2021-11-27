87ee44710b757905058d932d86b13b10dc8da242
Max <78352628+Max-Prasertsan@users.noreply.github.com>
2021-11-27 12:19:11 +0000

    Update benchmark_test.go

--- gol-skeleton-master/benchmark_test.go
+++ gol-skeleton-master/benchmark_test.go
@@ -2,25 +2,25 @@ package main
 
 import (
     "fmt"
-	"os"
 	//"runtime/trace"
 	"testing"
 	"uk.ac.bris.cs/gameoflife/gol"
 	//"uk.ac.bris.cs/gameoflife/util"
 )
-// TODO: go test -run=Bench -bench benchmark_test
-func benchMark(t *testing.T){
-    os.Stdout = nil
-    p := gol.Params{ImageWidth: 512, ImageHeight: 512, Turns: 1000}
+// TODO: go test -run=BenchmarkGol -v -bench .
+func BenchmarkGol(t *testing.B){
+    p := gol.Params{ImageWidth: 512, ImageHeight: 512, Turns: 100, Threads: 16}
     for threads := 1; threads <= 16; threads ++{
         p.Threads = threads
         testName := fmt.Sprintf("%dx%dx%d-%d", p.ImageWidth, p.ImageHeight, p.Turns, p.Threads)
-        t.Run(testName, func(t *testing.T){
-	// Issue with t.N. Need TA help later	
-            for i := 0; i < t.N; i++ {
-                events := make(chan gol.Event)
-                go gol.Run(p, events, nil)
-            }
+        t.Run(testName, func(t *testing.B){
+           for i := 0; i < p.Turns; i++ {
+               fmt.Println("after turns")
+               fmt.Println(i)
+               events := make(chan gol.Event)
+               go gol.Run(p, events, nil)
+           }
         })
     }
 }
+
